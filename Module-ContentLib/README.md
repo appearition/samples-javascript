08426e089cd9be68e3c650a1d301da12362511b0
Himanshu Gupta <thinktank.himanshu@gmail.com>
2023-03-16 22:37:38 +0530

    content lib sample: retrival logic implemented and ui updates

--- Module-ContentLib/README.md
+++ Module-ContentLib/README.md
@@ -1,7 +1,7 @@
-## Moudle-ContentLib
+# Moudle-ContentLib
 
 A sample project demostrating how to use the content libray module for managing assets.
 
-### Getting Started
+## Getting Started
 
 1. Clone the project
--- Module-ContentLib/script.js
+++ Module-ContentLib/script.js
@@ -2,6 +2,7 @@ import { createItem, uploadModel, getItem } from './utils/Appearition.js';
 
 const uploadButton = document.getElementById('upload-button');
 const retriveButton = document.getElementById('retrive-button');
+const retriveContainer = document.getElementById('retrive-container');
 
 const fileInput = document.getElementById('model-file');
 let file;
@@ -14,6 +15,9 @@ fileInput.addEventListener('change', (event) => {
 const processUpload = async (event) => {
   event.preventDefault();
 
+  if (!file) return;
+
+  uploadButton.textContent = 'Uploading...';
   const item = {
     ProviderName: 'InternalContentLibrary',
     Key: null,
@@ -29,13 +33,30 @@ const processUpload = async (event) => {
   formData.append('file', file);
 
   const data2 = await uploadModel(formData, fileKey);
+
+  if (data2) retriveContainer.style.display = 'flex';
   console.log(data2);
+  uploadButton.textContent = 'Upload';
 };
 
 const retriveItem = async (event) => {
   event.preventDefault();
+  retriveButton.textContent = 'Retrieving...';
   const data = await getItem(fileKey);
   console.log(data.Data.Files[0].Url);
+
+  let modelViewer = `<model-viewer
+  src=${data.Data.Files[0].Url}
+  ar
+  camera-controls
+  touch-action="pan-y"
+></model-viewer>`;
+
+  let viewerElement = document.createElement('div');
+  viewerElement.innerHTML = modelViewer;
+
+  retriveButton.insertAdjacentElement('afterend', viewerElement.firstChild);
+  retriveButton.style.display = 'none';
 };
 
 uploadButton.addEventListener('click', processUpload);
